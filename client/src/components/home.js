088b82a3e973978dddbc322183622eaae99fb1e3
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-02-26 21:12:25 +0530

    Fix new,js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,60 +1,57 @@
-import Layout from '../components/Layout'
-import React,{ useState,useEffect } from 'react'
-import 'semantic-ui-css/semantic.min.css'
-import { Card,Button } from 'semantic-ui-react';
-import { Link } from "react-router-dom";
+import Layout from '../components/Layout';
+import React, { useState, useEffect } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { Card, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import Manager from './../ethereum/manager';
 
 function App() {
-
-	const [auction,getAuctions] = useState({
+	const [auction, getAuctions] = useState({
 		add: [],
-		details: []
-	})
-	
-	useEffect(()=>{
-		const getAuctionDetails = async() => {
+		details: [],
+	});
+
+	useEffect(() => {
+		const getAuctionDetails = async () => {
 			const auctions = await Manager.methods.getAuctions().call();
 			const auctionsdetails = await Manager.methods.getDetails().call();
 			getAuctions({
-				add:auctions,
-				details:auctionsdetails
-			})
-		}
+				add: auctions,
+				details: auctionsdetails,
+			});
+		};
 		getAuctionDetails();
-	},[])
-  	
-	
+	}, []);
 
-    return (
-      <Layout>
-        <div>
-          	<Link to="/new">
-				<p>
-					<Button floated="right"
-						content="New Auction"
-						icon="add circle"
-						primary={true}
-					/>
-				</p>
-			</Link>
-			<Card.Group items={
-				auction.add.map((address,i) => {
-					return {
-						header: auction.details[i],
-						description: (
-						  <Link to={`/auction/${address}`}>
-							<p>View Auction</p>
-						</Link>
-						),
-						fluid: true
-					};
-				})
-			} />
-        </div>
-      </Layout>
-    )
-
+	return (
+		<Layout>
+			<div>
+				<Link to="/new">
+					<p>
+						<Button
+							floated="right"
+							content="New Auction"
+							icon="add circle"
+							primary={true}
+						/>
+					</p>
+				</Link>
+				<Card.Group
+					items={auction.add.map((address, i) => {
+						return {
+							header: auction.details[i],
+							description: (
+								<Link to={`/auction/${address}`}>
+									<p>View Auction</p>
+								</Link>
+							),
+							fluid: true,
+						};
+					})}
+				/>
+			</div>
+		</Layout>
+	);
 }
 
 export default App;
--- client/src/components/new.js
+++ client/src/components/new.js
@@ -1,63 +1,61 @@
-import Layout from './Layout'
-import React,{ useState } from 'react'
-import 'semantic-ui-css/semantic.min.css'
-import { Form,Input,Button,Message } from 'semantic-ui-react'
+import Layout from './Layout';
+import React, { useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { Form, Input, Button, Message } from 'semantic-ui-react';
 import Manager from '../ethereum/manager';
 import web3 from '../ethereum/web3';
-import { Redirect } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 
 const AuctionPage = () => {
-  const [newAucDetail,setDetail] = useState({
-    amount: '',
-    detail: '',
-    loading: false,
-    errorMessage: '',
-    url: '/new'
-  })
+	const [Detail, setDetail] = useState('');
+	const [errorM, setError] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [redirect, setRedirect] = useState(false);
+	const [amount, setAmount] = useState('');
 
-  const onSubmit = async () => {
-    setDetail({ loading: true })
-    try {
-      const accounts = await web3.eth.requestAccounts();
-      await Manager.methods.createAuction(this.state.amount, this.state.detail).send({
-        from: accounts[0]
-      });
-      //Router.pushRoute('/');
-      setDetail({ url: '/' })
-    } catch (e) {
-      setDetail({ errorMessage: e.message});
-    } 
+	const onSubmit = async (e) => {
+		setLoading(true);
+		try {
+			const accounts = await web3.eth.requestAccounts();
+			await Manager.methods.createAuction(amount, Detail).send({
+				from: accounts[0],
+			});
+			setRedirect(true);
+		} catch (e) {
+			setError(e.message);
+			console.log(e);
+		}
+		setLoading(false);
+	};
 
-    setDetail({ loading: false })
-  }
-
-  return (
-    <Layout>
-      <Redirect to={newAucDetail.url} />
-      <div>
-      <Form onSubmit={onSubmit} error={!!newAucDetail.errorMessage}>
-        <Form.Field>
-        <label>Auction details</label>
-        <Input
-          placeholder='Base price'
-          value={newAucDetail.amount}
-          onChange={event => setDetail({ amount: event.target.value })}
-          label="ether" labelPosition="right"
-        />
-        <Input
-          placeholder='Spectrum detail'
-          value={newAucDetail.detail}
-          onChange={event => setDetail({ detail: event.target.value })}
-        />
-      </Form.Field>
-      <Message error header="Oops!" content={newAucDetail.errorMessage} />
-      <Button primary loading={newAucDetail.loading}>
-        Create Auction
-      </Button>
-      </Form>
-      </div>
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<div>
+				{redirect ? <Redirect push to="/" /> : null}
+				<Form onSubmit={onSubmit} error={!!errorM}>
+					<Form.Field>
+						<label>Auction details</label>
+						<Input
+							placeholder="Base price"
+							value={amount}
+							onChange={(event) => setAmount(event.target.value)}
+							label="ether"
+							labelPosition="right"
+						/>
+						<Input
+							placeholder="Spectrum detail"
+							value={Detail}
+							onChange={(event) => setDetail(event.target.value)}
+						/>
+					</Form.Field>
+					<Message error header="Oops!" content={errorM} />
+					<Button primary loading={loading}>
+						Create Auction
+					</Button>
+				</Form>
+			</div>
+		</Layout>
+	);
+};
 
 export default AuctionPage;
